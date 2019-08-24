562f08cc1a0845349d4a24f11a78f30c85d97a11
Jeremy <matsonjjl@gmail.com>
2019-08-24 14:00:12 -0700

    feat: Initial commit

---
+++ video_capture.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+img_index = 0
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Perform operations on the frame
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the modified frame
+    #cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1)
+    #if key != -1:
+    #    print('Key:', key)
+
+    if key == 27 or key == ord('q'):
+        break
+    elif key == ord('s'):
+        cv2.imwrite('pictures/image' + str(img_index) + '.jpeg', frame)
+        img_index += 1
+        print('Created image:', img_index)
+
+# Release the resources
+cap.release()
+cv2.destroyAllWindows()
